b57bb06b62e01c2e3e5d71e2f0bb0bc0e91affee
anthony <gachinigaithuma@gmail.com>
2023-10-04 15:59:33 +0300

    Completed

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
 // Write your code in this file!
+const currentUser = 'Anthony Gachini';
+
+const welcomeMessage = 'Welcome to Flatbook, ' + currentUser + '!';
+
+const excitedWelcomeMessage = welcomeMessage.toUpperCase();
+
+const firstInitial = currentUser.charAt(0);
+const shortGreeting = 'Welcome, ' + firstInitial + '!';
+
+
